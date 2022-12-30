4c6d05decb93d25027ce031007a767963988db97
zahmis <meda829ratteru86@gmail.com>
2022-12-31 01:12:44 +0900

    FIXED

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
 import styled from "styled-components";
 import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
-    <>
-      <Main>
-        <Navbar />
-      </Main>
-    </>
+    <Main>
+      <Navbar />
+    </Main>
   );
 }
 
