8851f163115fc2cd34ff1b56318a854e6499ad6b
Ketuago <shiro_negi39@yahoo.co.jp>
2023-01-04 23:00:49 +0900

    Navbar-update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Image from "next/image";
 
 export const Navbar = () => {
   return (
     <NavbarContainer>
+      <NavbarLogo>
+        <Image
+          src=""
+          alt="proteinus-logo"
+          width={60}
+          height={60}
+          style={{
+            backgroundColor: "#797979",
+            flexGrow: "1",
+          }}
+        />
+        <NavbarLink> Proteinus</NavbarLink>
+      </NavbarLogo>
       <NavbarLinkContainer>
-        <NavbarLink> Home</NavbarLink>
         <NavbarLink> Crafts</NavbarLink>
         <NavbarLink> Future</NavbarLink>
         <NavbarLink> About Us</NavbarLink>
@@ -18,15 +31,20 @@ export const Navbar = () => {
 
 const NavbarContainer = styled.nav`
   width: 100%;
-  background-color: #bf7722;
   opacity: 56%;
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const NavbarLogo = styled.div`
+  display: flex;
+  justify-content: flex-start;
 `;
 
 const NavbarLinkContainer = styled.div`
   display: flex;
-  justify-content: right;
+  margin-right: 120px;
+  justify-content: flex-end;
 `;
 
 export const NavbarLink = styled.div`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
-import { Navbar } from '../components/Navbar';
-import Image from 'next/image';
+import styled from "styled-components";
+import { Navbar } from "../components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,58 +9,58 @@ export default function Home() {
         <Navbar />
         <ImageGrid>
           <Image
-            src='/cg1.PNG'
-            alt='cg1'
+            src="/cg1.PNG"
+            alt="cg1"
             width={537}
             height={666}
             style={{
-              objectFit: 'cover',
+              objectFit: "cover",
             }}
           />
           <Image
-            src='/cg2.PNG'
-            alt='cg2'
+            src="/cg2.PNG"
+            alt="cg2"
             width={560}
             height={507}
             style={{
-              objectFit: 'cover',
+              objectFit: "cover",
             }}
           />
           <Image
-            src=''
-            alt='3'
+            src=""
+            alt="3"
             width={1203}
             height={533}
             style={{
-              backgroundColor: '#797979',
-              objectFit: 'cover',
+              backgroundColor: "#797979",
+              objectFit: "cover",
               gridRow: 2,
               gridColumnStart: 1,
               gridColumnEnd: 3,
             }}
           />
           <Image
-            src=''
-            alt='4'
+            src=""
+            alt="4"
             width={422}
             height={524}
             style={{
-              backgroundColor: '#797979',
-              objectFit: 'cover',
-              alignSelf: 'start',
+              backgroundColor: "#797979",
+              objectFit: "cover",
+              alignSelf: "start",
               gridRow: 3,
               gridColumn: 1,
             }}
           />
           <Image
-            src=''
-            alt='5'
+            src=""
+            alt="5"
             width={705}
             height={1045}
             style={{
-              backgroundColor: '#797979',
-              objectFit: 'cover',
-              justifySelf: 'end',
+              backgroundColor: "#797979",
+              objectFit: "cover",
+              justifySelf: "end",
               gridRowStart: 3,
               gridRowEnd: 5,
               gridColumnStart: 2,
@@ -68,13 +68,13 @@ export default function Home() {
             }}
           />
           <Image
-            src=''
-            alt='6'
+            src=""
+            alt="6"
             width={422}
             height={390}
             style={{
-              backgroundColor: '#797979',
-              objectFit: 'cover',
+              backgroundColor: "#797979",
+              objectFit: "cover",
               gridRow: 4,
               gridColumn: 1,
             }}
